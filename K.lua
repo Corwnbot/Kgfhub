80659761c71a418a7fc3a43b3bfaf4da783c46f4
Corwnbot <123889850+Corwnbot@users.noreply.github.com>
2024-06-24 17:23:36 +0300

    Update K.lua

--- K.lua
+++ K.lua
@@ -1,41 +1,12 @@
+if game.PlaceId == 2753915549 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "KGF HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "KGFHUB"})
 
 -- Local 
-local args = {
+local ken = {
     [1] = "Ken",
     [2] = true
 }
-
-local SKELETON = {
-    [1] = "hit",
-    [2] = {
-        [1] = workspace.Enemies:FindFirstChild("Reborn Skeleton").HumanoidRootPart,
-        [2] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightUpperArm,
-        [3] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightLowerArm,
-        [4] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightHand,
-        [5] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightFoot,
-        [6] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightLowerLeg,
-        [7] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightUpperLeg,
-        [8] = workspace.Enemies:FindFirstChild("Reborn Skeleton").HumanoidRootPart,
-        [9] = workspace.Enemies:FindFirstChild("Reborn Skeleton").Head,
-        [10] = workspace.Enemies:FindFirstChild("Reborn Skeleton").UpperTorso,
-        [11] = workspace.Enemies:FindFirstChild("Reborn Skeleton").LowerTorso,
-        [12] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightUpperArm,
-        [13] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightLowerArm,
-        [14] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightHand,
-        [15] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightFoot,
-        [16] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightLowerLeg,
-        [17] = workspace.Enemies:FindFirstChild("Reborn Skeleton").RightUpperLeg,
-        [18] = workspace.Enemies:FindFirstChild("Reborn Skeleton").LeftUpperLeg,
-        [19] = workspace.Enemies:FindFirstChild("Reborn Skeleton").LeftUpperArm,
-        [20] = workspace.Enemies:FindFirstChild("Reborn Skeleton").LeftLowerArm,
-        [21] = workspace.Enemies:FindFirstChild("Reborn Skeleton").LeftHand
-    },
-    [3] = 1,
-    [4] = ""
-}
-
 
 -- Tabs
 -- Bone
@@ -58,16 +29,16 @@ AutoBone:AddToggle({
 	Name = "Auto Farm Bone",
 	Default = false,
 	Callback = function(Value)
-		game:GetService("ReplicatedStorage").RigControllerEvent:FireServer(unpack(args))
+		print(Value)
 	end    
 })
 
 -- Fast Attack
 Setting:AddToggle({
-	Name = "Fast Attack",
+	Name = "Auto Ken Haki",
 	Default = false,
 	Callback = function(Value)
-		print(Value)
+		game:GetService("ReplicatedStorage").RigControllerEvent:FireServer(unpack(ken))
 	end    
 })
 
