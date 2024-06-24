3e00a98316e03c8da3fca607088077454b831408
Corwnbot <123889850+Corwnbot@users.noreply.github.com>
2024-06-24 23:27:51 +0300

    Update K.lua

--- K.lua
+++ K.lua
@@ -1,13 +1,16 @@
 if game.PlaceId == 2753915549 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "KGF HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "KGFHUB"})
+-- Vlaues
+_G.RandomBone = true
 
--- Local 
-local ken = {
-    [1] = "Ken",
-    [2] = true
-}
-
+--Functions
+function RandomBone()
+while _G.RandomBone == true do
+   game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("getInventory")
+   wait(0.000001)
+  end
+end
 -- Tabs
 -- Bone
 local AutoBone = Window:MakeTab({
@@ -29,18 +32,11 @@ AutoBone:AddToggle({
 	Name = "Auto Farm Bone",
 	Default = false,
 	Callback = function(Value)
-		print(Value)
-	end    
+	_G.RandomBone = Value
+	RandomBone()
+end
 })
 
--- Fast Attack
-Setting:AddToggle({
-	Name = "Auto Ken Haki",
-	Default = false,
-	Callback = function(Value)
-		game:GetService("ReplicatedStorage").RigControllerEvent:FireServer(unpack(ken))
-	end    
-})
 
 end
 OrionLib:Init()
