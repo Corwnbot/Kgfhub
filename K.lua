fc305602ba78304c8ae91a4c252acd13eb76b30c
Corwnbot <123889850+Corwnbot@users.noreply.github.com>
2024-06-25 00:13:58 +0300

    Update K.lua

--- K.lua
+++ K.lua
@@ -1,69 +1,28 @@
-
+if game.Place.Id = 15705682243 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "KGF HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "KGFHUB"})
+local Window = OrionLib:MakeWindow({Name = "KGF TAP HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "KGFHUB"})
 -- Vlaues
 _G.Tap = true
+_G.reb = true
 
 --Functions
 function tap()
 while tap = true do
    game:GetService("ReplicatedStorage")["SSG Framework"].Shared.Network.tap:FireServer()
-   wait(0.000000]]]]]]0000000000000000001)
+   wait(0.00000000000000000000000000000001)
+   end
+end
+
+function r()
+while _G.reb = true do
+   game:GetService("ReplicatedStorage")["SSG Framework"].Shared.Network.rebirth:InvokeServer()
+   wait(0.00000000000000000000000000000001)
    end
 end
 -- Tabs
 -- Bone
 local AutoFarm = Window:MakeTab({
 	Name = "AutoFarm",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
--- Quest
-local Quest = Window:MakeTab({
-	Name = "Quest",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
--- Fruit / Raid
-local FruitRaid = Window:MakeTab({
-	Name = "Fruit & Raid",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
--- Farm Sea
-local FarmSea = Window:MakeTab({
-	Name = "Farm Sea",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
--- Races
-local Races = Window:MakeTab({
-	Name = "Races",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
--- Teleports
-local Teleports = Window:MakeTab({
-	Name = "Teleports",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
--- Shop
-local Shop = Window:MakeTab({
-	Name = "Shop",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
--- Setting
-local Setting = Window:MakeTab({
-	Name = "Setting",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
@@ -77,5 +36,14 @@ AutoFarm:AddToggle({
 		tap()
 	end    
 })
+AutoFarm:AddToggle({
+	Name = "Auto Rebirth",
+	Default = false,
+	Callback = function(Value)
+		_G.reb = Value
+		reb()
+	end    
+})
 
+end
 OrionLib:Init()
