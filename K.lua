9c25cc5bf5a2ceb692ef25ee4e58dd27ccab3577
Corwnbot <123889850+Corwnbot@users.noreply.github.com>
2024-06-25 00:02:47 +0300

    Update K.lua

--- K.lua
+++ K.lua
@@ -1,22 +1,62 @@
-if game.PlaceId == 7449423635 then
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "KGF HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "KGFHUB"})
 -- Vlaues
-_G.RandomBone = true
+_G.Tap = true
 
 --Functions
-function RandomBone()
-while _G.RandomBone == true do
-   
-   game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buso")
-
-   wait(0.000001)
-  end
+function tap()
+while tap = true do
+   game:GetService("ReplicatedStorage")["SSG Framework"].Shared.Network.tap:FireServer()
+   wait(0.000000]]]]]]0000000000000000001)
+   end
 end
 -- Tabs
 -- Bone
-local AutoBone = Window:MakeTab({
-	Name = "FarmBone",
+local AutoFarm = Window:MakeTab({
+	Name = "AutoFarm",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Quest
+local Quest = Window:MakeTab({
+	Name = "Quest",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Fruit / Raid
+local FruitRaid = Window:MakeTab({
+	Name = "Fruit & Raid",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Farm Sea
+local FarmSea = Window:MakeTab({
+	Name = "Farm Sea",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Races
+local Races = Window:MakeTab({
+	Name = "Races",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Teleports
+local Teleports = Window:MakeTab({
+	Name = "Teleports",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Shop
+local Shop = Window:MakeTab({
+	Name = "Shop",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
@@ -29,16 +69,13 @@ local Setting = Window:MakeTab({
 })
 
 -- Toggles
--- Auto Farm Bones
-AutoBone:AddToggle({
-	Name = "Auto Farm Bone",
+AutoFarm:AddToggle({
+	Name = "Auto TAP",
 	Default = false,
 	Callback = function(Value)
-	_G.RandomBone = Value
-	RandomBone()
-end
+		_G.tap = Value
+		tap()
+	end    
 })
 
-
-end
 OrionLib:Init()
