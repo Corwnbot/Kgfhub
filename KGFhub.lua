3f232ba248756b5717caedf8bcf2e600505ec58a
Corwnbot <123889850+Corwnbot@users.noreply.github.com>
2024-06-28 11:03:17 +0300

    Update KGFhub.lua

--- KGFhub.lua
+++ KGFhub.lua
@@ -21,17 +21,17 @@ function CheckQuest()
 	 CQ = CFrame.new(1062.64697265625, 16.516624450683594, 1546.55224609375)
 	 end
  end
- function PlayerTP(P)
+ function TP(P)
 	 Distance = (P.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
 	 if Distance < 10 then
 		 Speed = 1000
 	 elseif Distance < 170 then
 		 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = P
-		 Speed = 400
+		 Speed = 350
 	 elseif Distance < 1000 then
-		 Speed = 400
+		 Speed = 350
 	 elseif Distance >= 1000 then
-		 Speed = 400
+		 Speed = 300
 	 end
 	 game:GetService("TweenService"):Create(
 		 game.Players.LocalPlayer.Character.HumanoidRootPart,
@@ -42,16 +42,16 @@ function CheckQuest()
  
  spawn(function()
 	while task.wait() do
-		if getgenv().AutoFarm then
+		if _G.AutoFarm then
 			CheckQuest()
 			if game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == false then
-				 PlayerTP(CQ)
+				 TP(CQ)
 				 task.wait(0.9)
 				 game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StartQuest",NQ,LQ)
 				 elseif game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == true then
 					 for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
 						 if v.Name == Ms then
-						 PlayerTP(v.HumanoidRootPart.CFrame * CFrame.new(0,20,0))
+						 TP(v.HumanoidRootPart.CFrame * CFrame.new(0,20,0))
 						 v.HumanoidRootPart.Size = Vector3.new(60,60,60)
 					 end
 				 end
@@ -61,7 +61,7 @@ function CheckQuest()
  end)
  spawn(function()
     game:GetService("RunService").RenderStepped:Connect(function()
-		if getgenv().AutoFarm then
+		if _G.AutoFarm then
 		pcall(function()
                 game:GetService'VirtualUser':CaptureController()
 			    game:GetService'VirtualUser':Button1Down(Vector2.new(0,1,0,1))
@@ -106,23 +106,17 @@ end)
 
         MainSection:NewToggle("AutoFarm", "AutoFarm Test", function(state)
             if state then
-                getgenv().AutoFarm = true
+                _G.AutoFarm = true
             else
-                getgenv().AutoFarm = false
+                _G.AutoFarm = false
             end
         end)
+        
         ---Teleport
         local Teleport = Window:NewTab("Teleport")
         local TeleportSection = Teleport:NewSection("Teleport")
 
-        TeleportSection:NewButton("Mansion", "Teleport you there", function()
+        TeleportSection:NewButton("Pirate Island", "Teleport you there", function()
             tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-12471, 374, -7551)}):Play()
+tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1041.8861083984375, 16.273563385009766, 1424.93701171875)}):Play()
         end)
-
---[[
-```lua
-local humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid")
-
-game:GetService("TweenService"):Create(humanoid, TweenInfo.new(2), { Position = asfafag } ```
---[[
